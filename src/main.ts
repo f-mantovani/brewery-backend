351c2900039842413df716090877c99cc6cd2bba
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2023-09-28 00:11:08 +0200

    feature(add swagger basics): followed the nestjs website steps

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Felipe Brewery')
+    .setDescription('NestJS Basics')
+    .setVersion('0.1.0')
+    .addTag('beers')
+    .build();
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
     }),
   );
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
